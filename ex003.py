3e502dbbdc1c02646611c49532f914b0286e7a62
joaomarcosrs <joaomarcosrs40@gmail.com>
2022-08-21 11:48:50 -0300

    Somando dois números

--- ex003.py
+++ ex003.py
@@ -1,4 +1,5 @@
 num1 = int(input('Digite um número: '))
 num2 = int(input('Digite outro número: '))
 soma = num1 + num2
-print('A soma entre {} e {} é {}'.format(num1, num2, soma))
+
+print('\nA soma entre os números \033[31m{}\033[m e \033[31m{}\033[m é igual a \033[1;32m{}\033[m'.format(num1, num2, soma))
