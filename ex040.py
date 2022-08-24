efe7bb31ee8a14ef213c9fe53f4062d110a37cdf
João Marcos <88893617+joaomarcosrs@users.noreply.github.com>
2022-08-24 14:00:35 -0300

    Aquele clássico da Média

---
+++ ex040.py
@@ -0,0 +1,14 @@
+nota1 = float(input('Digite a primeira nota do aluno: '))
+nota2 = float(input('Digite a segunda nota do aluno: '))
+
+media = (nota1 + nota2) / 2
+print('\n')
+if media < 5.0:
+  print('''Média: {:.1f}
+Aluno Reprovado!'''.format(media))
+elif media >= 5.0 and media < 7.0:
+  print('''Média: {:.1f}
+Aluno em Recuperação!'''.format(media))
+else:
+  print('''Média: {:.1f}
+Aluno Aprovado!'''.format(media))
