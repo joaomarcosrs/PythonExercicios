0640fb2526eae182622dae8ff245da8f9cfe0b05
joaomarcosrs <joaomarcosrs40@gmail.com>
2022-08-28 19:19:13 -0300

    Conversor de Bases Numéricas

--- ex037.py
+++ ex037.py
@@ -12,7 +12,13 @@ if op == 1:
   print('O número convertido para Binário é: {}'.format(format(numero, 'b')))
 elif op == 2:
   print('O número convertido para Octal é: {}'.format(format(numero, 'o')))
-else:
+elif op == 3:
   numero = format(numero, 'x')
   numero = str(numero)
   print('O número convertido para Hexadecimal é: {}'.format(numero.upper()))
+else:
+  print('Opção inválida! Tente novamente.')
+
+
+#Nesse código pode-se usar os métodos bin(), oct() e hex(), aconpnhados do fatiamento de strings [2:]
+# para apresentar os números sem as duas letras iniciais que indicam o tipo de conversão.
