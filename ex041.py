5f56e740f7ed1b868b2c21e053abef632146e81b
joaomarcosrs <joaomarcosrs40@gmail.com>
2022-08-28 20:42:38 -0300

    Classificando Atletas

--- ex041.py
+++ ex041.py
@@ -3,14 +3,16 @@ from datetime import date
 ano_nasc = int(input('Digite o ano de nascimento do atleta: '))
 ano_corr = date.today().year
 ano_diff = ano_corr - ano_nasc
+
 print('\n')
+print('O alteta possui {} anos'.format(ano_diff))
 if ano_diff <= 9:
-  print('O atleta tem {} anos e pertence à categoria MIRIM!'.format(ano_diff))
-elif ano_diff > 9 and ano_diff <= 14:
-  print('O atleta tem {} anos e pertence à categoria INFANTIL!'.format(ano_diff))
-elif ano_diff > 14 and ano_diff <= 19:
-  print('O atleta tem {} anos e pertence à categoria JÚNIOR!'.format(ano_diff))
-elif ano_diff > 19 and ano_diff <=20:
-  print('O atleta tem {} anos e pertence à categoria SÊNIOR!'.format(ano_diff))
+  print('Categoria MIRIM!')
+elif ano_diff <= 14:
+  print('Categoria INFANTIL!')
+elif ano_diff <= 19:
+  print('Categoria JÚNIOR!')
+elif ano_diff <=20:
+  print('Categoria SÊNIOR!')
 else:
-  print('O atleta tem {} anos e pertence à categoria MASTER!'.format(ano_diff))
+  print('Categoria MASTER!')
