a02e814f197810d040c48da85554950d88d8dda9
joaomarcosrs <joaomarcosrs40@gmail.com>
2022-08-28 15:32:20 -0300

    GAME: Pedra Papel e Tesoura

--- ex045.py
+++ ex045.py
@@ -1,8 +1,9 @@
 from random import randint
 
+
 def jokenpo():
-  print('\n\nVamos jogar Jokenpô?')
-  opcao_msg = '''
+    print('\n\nVamos jogar Jokenpô?')
+    opcao_msg = '''
 Escolha uma opção:
 
 [ 1 ] PEDRA
@@ -12,38 +13,39 @@ Escolha uma opção:
 
 Já escolhi o meu!!!
 '''
-  opcao = int(input(opcao_msg))
-  def cond_inicial():
-    global cond_escolha
-    cond_escolha = opcao == 1 or opcao == 2 or opcao == 3
+    opcao = int(input(opcao_msg))
 
-  def cond_game():
-    global cond_empate
-    global cond_vitoria
-    global cond_derrota
-    cond_empate = escolha_pc == 'Pedra' and opcao == 1 or escolha_pc == 'Papel' and opcao == 2 or escolha_pc == 'Tesoura' and opcao == 3
-    cond_vitoria = escolha_pc == 'Pedra' and opcao == 3 or escolha_pc == 'Papel' and opcao == 1 or escolha_pc == 'Tesoura' and opcao == 2
-    cond_derrota = escolha_pc == 'Pedra' and opcao == 2 or escolha_pc == 'Papel' and opcao == 3 or escolha_pc == 'Tesoura' and opcao == 1
+    def cond_inicial():
+        global cond_escolha
+        cond_escolha = opcao == 1 or opcao == 2 or opcao == 3
 
-  if opcao == 0:
-    print('\nMuito Obrigado por jogar comigo. Até a próxima!')
-  elif cond_escolha:
-      jo_list = ['Pedra', 'Papel', 'Tesoura']
-      i = randint(0, 2)
-      escolha_pc = jo_list[i]
-      cond_game()
+    def cond_game():
+        global cond_empate
+        global cond_vitoria
+        global cond_derrota
+        cond_empate = escolha_pc == 'Pedra' and opcao == 1 or escolha_pc == 'Papel' and opcao == 2 or escolha_pc == 'Tesoura' and opcao == 3
+        cond_vitoria = escolha_pc == 'Pedra' and opcao == 3 or escolha_pc == 'Papel' and opcao == 1 or escolha_pc == 'Tesoura' and opcao == 2
+        cond_derrota = escolha_pc == 'Pedra' and opcao == 2 or escolha_pc == 'Papel' and opcao == 3 or escolha_pc == 'Tesoura' and opcao == 1
 
-      if cond_empate:
-        print('\nEMPATAMOS! Eu também escolhi {}'.format(escolha_pc))
-        print('Vamos de novo!')
-        jokenpo()
-      elif cond_vitoria:
-        print('\nVenci!!! Eu escolhi {}'.format(escolha_pc))
-        print('Você quer jogar de novo?')
-        jokenpo()
-      elif cond_derrota:
-        print('\nPerdi!!! Eu escolhi {}'.format(escolha_pc))
-        print('Me dê outra chance...')
-        jokenpo()
+    if opcao == 0:
+        print('\nMuito Obrigado por jogar comigo. Até a próxima!')
+    elif cond_escolha:
+        jo_list = ['Pedra', 'Papel', 'Tesoura']
+        i = randint(0, 2)
+        escolha_pc = jo_list[i]
+        cond_game()
+        if cond_empate:
+            print('\nEMPATAMOS! Eu também escolhi {}'.format(escolha_pc))
+            print('Vamos de novo!')
+            jokenpo()
+        elif cond_vitoria:
+            print('\nVenci!!! Eu escolhi {}'.format(escolha_pc))
+            print('Você quer jogar de novo?')
+            jokenpo()
+        elif cond_derrota:
+            print('\nPerdi!!! Eu escolhi {}'.format(escolha_pc))
+            print('Me dê outra chance...')
+            jokenpo()
+
 
 jokenpo()
