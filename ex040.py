36859fd7b89a23916a8225160448d5f520b7fee3
joaomarcosrs <joaomarcosrs40@gmail.com>
2022-08-28 20:03:50 -0300

    Aquele clássico da Média

--- ex040.py
+++ ex040.py
@@ -6,7 +6,7 @@ print('\n')
 if media < 5.0:
   print('''Média: {:.1f}
 Aluno Reprovado!'''.format(media))
-elif media >= 5.0 and media < 7.0:
+elif media >= 5.0 and media < 7.0:  #Pode-se usar a sintaxe 7.0 > media <= 5.0
   print('''Média: {:.1f}
 Aluno em Recuperação!'''.format(media))
 else:
