024fa4a87a59b001894cfa095e9f7c1ed7b590ad
João Marcos <88893617+joaomarcosrs@users.noreply.github.com>
2022-08-30 12:57:39 -0300

    GAME: Pedra Papel e Tesoura

--- ex045.py
+++ ex045.py
@@ -1,6 +1,5 @@
 from random import randint
 
-
 def jokenpo():
     print('\n\nVamos jogar Jokenpô?')
     opcao_msg = '''
@@ -14,11 +13,9 @@ Escolha uma opção:
 Já escolhi o meu!!!
 '''
     opcao = int(input(opcao_msg))
-
     def cond_inicial():
         global cond_escolha
         cond_escolha = opcao == 1 or opcao == 2 or opcao == 3
-
     def cond_game():
         global cond_empate
         global cond_vitoria
@@ -26,7 +23,6 @@ Já escolhi o meu!!!
         cond_empate = escolha_pc == 'Pedra' and opcao == 1 or escolha_pc == 'Papel' and opcao == 2 or escolha_pc == 'Tesoura' and opcao == 3
         cond_vitoria = escolha_pc == 'Pedra' and opcao == 3 or escolha_pc == 'Papel' and opcao == 1 or escolha_pc == 'Tesoura' and opcao == 2
         cond_derrota = escolha_pc == 'Pedra' and opcao == 2 or escolha_pc == 'Papel' and opcao == 3 or escolha_pc == 'Tesoura' and opcao == 1
-
     if opcao == 0:
         print('\nMuito Obrigado por jogar comigo. Até a próxima!')
     elif cond_escolha:
@@ -46,6 +42,4 @@ Já escolhi o meu!!!
             print('\nPerdi!!! Eu escolhi {}'.format(escolha_pc))
             print('Me dê outra chance...')
             jokenpo()
-
-
 jokenpo()
