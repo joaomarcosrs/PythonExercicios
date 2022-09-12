f89886cce88519719970270eb0bb109e6d0ddce2
João Marcos <88893617+joaomarcosrs@users.noreply.github.com>
2022-09-12 12:06:52 -0300

    Soma ímpares múltiplos de três

--- ex048.py
+++ ex048.py
@@ -3,4 +3,5 @@ soma = 0
 for i in range(1, 501, 2):
      if i % 3 == 0:
          soma = soma + i
-print(f'A soma de todos os valores é {soma}')
+         contador = contador + 1
+print(f'A soma de todos os {contador} valores é {soma}')
