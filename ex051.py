81cee873a6fa06cd5572ec1ca7401c173a8a95f4
João Marcos <88893617+joaomarcosrs@users.noreply.github.com>
2022-09-12 14:08:02 -0300

    Progressão Aritmética

--- ex051.py
+++ ex051.py
@@ -6,7 +6,6 @@ print(
     f'{"Progressão Aritmética":^40}\n'
     f'{"="*40}\n')
 
-
 print(f'O 1° termo é: {pri_termo:>3}')
 print(f'A razão é: {razao:>6}\n')
 print(f'Os 10 primeiros termos da PA são:\n')
@@ -15,3 +14,13 @@ print(pri_termo, end=' ')
 for i in range(0, 9):
   termo = termo + razao
   print(f'{termo}', end =' ')
+
+
+
+# # Solução do Professor #
+# primeiro = int(input('Primeiro termo: '))
+# razao = int(input('Razão: '))
+# decimo =  primeiro + (10 - 1) * razao
+# for c in range(primeiro, decimo + razao, razao):
+#   print(f'{c}', end=' → ')
+# print('ACABOU')
