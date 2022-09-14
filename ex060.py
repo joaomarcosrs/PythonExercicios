14625cf31d2d1213c8322b4ecf6f6bf8c4ab70e7
João Marcos <88893617+joaomarcosrs@users.noreply.github.com>
2022-09-14 14:29:30 -0300

    Cálculo do Fatorial

--- ex060.py
+++ ex060.py
@@ -1,10 +1,30 @@
-print(f'\n{"-=-"*6}Cálculo do Fatorial{"-=-"*6}')
+###### FATORIAL USANDO WHILE #######
+print(f'\n{"-=-"*6}Cálculo de Fatorial{"-=-"*6}\n')
 numero = int(input('Digite um número: '))
-# i = numero
+lst_fatorial = []
 fatorial = 1
+i = numero
+
 while numero > 0:
     fatorial = numero * fatorial
+    lst_fatorial.append(str(numero))
     numero = numero - 1
-print(fatorial)
+
+print(f'\nO cálcullo de {i}! é...\n'
+      f'\n{i}! = {" x ".join(lst_fatorial)} = {fatorial}')
+
+
+# ###### FATORIAL USANDO FOR #######
+# print(f'\n{"-=-"*6}Cálculo de Fatorial {"-=-"*6}\n')
+# numero = int(input('Digite um número: '))
+
+# lst_fatorial = []
+# fatorial = 1
+# for i in range(numero, 0, -1):
+#   fatorial = i * fatorial
+#   lst_fatorial.append(str(i))
+
+# print(f'\nO cálcullo de {numero}! é...\n'
+#       f'\n{numero}! = {" x ".join(lst_fatorial)} = {fatorial}')
 
 
