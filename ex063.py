f43d6050607e3d7ef0ed4663d113708ce25840a1
joaomarcosrs <joaomarcosrs40@gmail.com>
2022-09-14 23:08:19 -0300

    em processo

--- ex063.py
+++ ex063.py
@@ -1,2 +1,15 @@
 numero = int(input('Digite um número inteiro: '))
+i = 0
+c = 1
+soma = 0
+
+while soma <= numero:
+    soma = i + c
+    n_soma = soma + c
+    c = n_soma
+    print(f'{soma}')
+# 1 = 0 + 1
+# 2 = 1 + 1
+# 3 = 2 + 1
+# 5 = 3 + 2
 
