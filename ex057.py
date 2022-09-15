155eecc267e3d7e92d76245916931c198188ff61
João Marcos <88893617+joaomarcosrs@users.noreply.github.com>
2022-09-15 14:34:37 -0300

    Validação de Dados

--- ex057.py
+++ ex057.py
@@ -9,3 +9,8 @@ else:
   print('O sexo digitado foi Feminino!')
 print('FIM')
 
+# ###################### Solução do Professor ###########################
+# sexo = str(input('Informe seu sexo [M/F]: ')).strip().upper()[0]
+# while sexo not in 'MmFf':
+#     sexo = str(input('Dados Iválidos. Por favor, infrme seu sexo: ')).strip().upper()[0]
+# print(f'Sexo {sexo} regstrado com sucesso!')
