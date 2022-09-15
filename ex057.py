4ba61b0e50ad090f05d804df806fe542da1b4575
João Marcos <88893617+joaomarcosrs@users.noreply.github.com>
2022-09-15 14:28:03 -0300

    Validação de Dados

--- ex057.py
+++ ex057.py
@@ -1,6 +1,6 @@
 sexo = ''
 while sexo != 'M' and sexo != 'F':
-    sexo = str(input('Digite o sexo [M/F]: ')).upper().strip()
+    sexo = str(input('Digite o sexo [M/F]: ')).upper().strip()[0]
     if sexo != 'M' and sexo != 'F':
         print('ERRO! Digite (M) para masculino e (F) para feminino')
 if sexo == 'M':
