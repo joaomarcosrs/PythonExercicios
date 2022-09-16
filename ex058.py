8befb67eb72ca241ab8726eaed2a2d63d7a116ec
João Marcos <88893617+joaomarcosrs@users.noreply.github.com>
2022-09-16 12:23:16 -0300

    Jogo da Adivinhação v2.0

--- ex058.py
+++ ex058.py
@@ -20,3 +20,22 @@ while num_random != num_digit:
     else:
         print(f'Menos... Tente novamente')
 print(f'Você precisou de {i} tentativa(s)')
+
+
+# ##################### SOLUÇÃO PROFESSOR ############################
+# computador = randint(0, 10)
+# print('Sou seu computador... Acabei de pensar em um número entre 0 e 10.')
+# print('Será que você consegue adivinhar qual foi? ')
+# acertou = false
+# palpites = 0
+# while not acertou:
+#     jogador = int(input('Qual é o seu palpite? '))
+#     palpites = palpites + 1
+#     if jogador == computador:
+#         acertou = true
+#     else:
+#         if jogador < computador:
+#             print('Mais... Tente mais uma vez.')
+#         elif jogador > computador:
+#             print('Menos... Tente mais uma vez.')
+# print(f'Acertou com {palpites} tentativas. Parabéns!')
