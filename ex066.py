2de499febf866733b286f2e34fb58998338df34d
João Marcos <88893617+joaomarcosrs@users.noreply.github.com>
2022-09-20 15:44:00 -0300

    Vários números com flag

---
+++ ex066.py
@@ -0,0 +1,8 @@
+soma = i = 0
+while True:
+    numero = int(input('Digite um número (ou digite 999 para parar): '))
+    if numero == 999:
+        break
+    soma = soma + numero
+    i = i + 1
+print(f'A soma dos {i} valores é {soma}')
