80552a2a11b417788050d7b77d42b33de02c46ef
João Marcos <88893617+joaomarcosrs@users.noreply.github.com>
2022-09-21 11:42:28 -0300

    Tabuada v3.0

---
+++ ex067.py
@@ -0,0 +1,26 @@
+numero = 0
+while numero >= 0:
+    numero = int(input('Deseja ver a tabuada de que valor? '))
+    if numero < 0:
+        print('PROGRAMA ENCERRADO! Volte Sempre!')
+        break
+
+    ### SOMA ###
+    print(f'\n{"~" * 3} SOMA {"~" * 3}')
+    for i in range(0, 11):
+        print(f'{i:>2} + {numero} = {i + numero:>2}')
+
+    ### SUBTRAÇÃO ###
+    print(f'\n{"~" * 3} SUBTRAÇÃO {"~" * 3}')
+    for i in range(0, 11):
+        print(f'{i:>2} - {numero} = {i - numero:>2}')
+
+    ### MULTIPLICAÇÃO ###
+    print(f'\n{"~" * 3} MULTIPLICAÇÃO {"~" * 3}')
+    for i in range(0, 11):
+        print(f'{i:>2} * {numero} = {i * numero:>2}')
+
+    ### Divisão ###
+    print(f'\n{"~" * 3} DIVISÃO {"~" * 3}')
+    for i in range(0, 11):
+        print(f'{i:>2} / {numero} = {i / numero:>2}')
