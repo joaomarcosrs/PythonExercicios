693a9e8b04cf1cc033700c01980420fbb72ba745
João Marcos <88893617+joaomarcosrs@users.noreply.github.com>
2022-09-22 12:50:56 -0300

    Jogo do Par ou Ímpar

--- ex068.py
+++ ex068.py
@@ -5,46 +5,56 @@ from random import randint
 print(f'{"-=-" * 20}\n'
       f'{"VAMOS JOGAR PAR OU ÍMPAR":^60}\n'
       f'{"-=-" * 20}\n')
-#Esse contador é
+#Atribui-se um contador para calcular quantas vezes será necessário até o usuário perder
 i = 0
+#inicío do laço de jogo
 while True:
+    #entrada do jogador
     jogador = int(input('Digite um número: '))
 
+    #atribuição 'False' para a variável do palpite para a forçar a escolha do usuário entre P e I
     palpite = 'False'
+    #inicío do laço para condicionar a escolha até o usuário escolher P ou I
     while palpite == 'False':
+        #entrada do usuário entre P ou I, note que ignora-se os espaços, trabalha-se com maiúsculas e pega-se apenas a primeira letra
         palpite = str(input('Par ou Ímpar [P/I]? ')).strip().upper()[:1]
+        #Inicío da condição para tratamento de opções inválidas
         if palpite not in 'PI':
             print(f'\nOPÇÃO INVÁLIDA'
                   f'\nEscolha (P) para PAR ou (I) para ÍMPAR\n')
             palpite = 'False'
         else:
+            #caso as opções corretas sejam digitadas corretamente sai do laço
             break
-
+    #gera-se um número aleatótio entre 0 e 10 para o palpite do computador
     computador = randint(0, 10)
+    #soma-se o número do jogador e do computador
     soma = computador + jogador
-
+    #teste simples para saber se é Par ou Ímpar
     if soma % 2 == 0:
         resultado = 'PAR'
     else:
         resultado = 'ÍMPAR'
-
+    #Mensagem ao usuário contendo os palpites, soma e resultado
     print(f'{"-" * 60}'
           f'\nVocê jogou {jogador} e o computador {computador}. Total de {soma} DEU {resultado}!\n'
           f'{"-" * 60}')
-
+    #teste para saber se o usuário ou o computador venceu a partida
+    #Caso o usuário vença, o jogo continua e o contador adiciona uma volta
     if soma % 2 == 0 and palpite in 'P' or soma % 2 != 0 and palpite in 'I':
         print(f'Você VENCEU!!!'
               f'\nVamos jogar novamente...\n')
         i = i + 1
     else:
+        #Caso jogador perca o laço é interrompido
         print(f'Você PERDEU!!!')
         break
-
-    if i == 1:
-        plu_sing = 'vez'
-    else:
-        plu_sing = 'vezes'
-
+#pequena condição só para corrigir o plural
+if i == 1:
+    plu_sing = 'vez'
+else:
+    plu_sing = 'vezes'
+#mensagem fina contado quantas partidas o usuário venceu
 print(f'{"-=-" * 20}\n'
       f'GAME OVER! Você venceu {i} {plu_sing}.\n'
       f'{"-=-" * 20}\n')
