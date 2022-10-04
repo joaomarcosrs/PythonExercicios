e5623620b85a768fa676b1b7d4552f9f2fe7d5d5
João Marcos <88893617+joaomarcosrs@users.noreply.github.com>
2022-10-04 14:40:04 -0300

    Análise de dados em uma Tupla

--- ex075.py
+++ ex075.py
@@ -1,8 +1,14 @@
 tpl_numero = ()
+tpl_numero_par = ()
+c = 0
 
 for i in range(0, 4):
-  numero = (int(input('Digite um número: ')), )
-  tpl_numero = tpl_numero + numero
+    numero = (int(input('Digite um número: ')),)
+    tpl_numero = tpl_numero + numero
+
+    if tpl_numero[i] % 2 == 0:
+        tpl_numero_par = tpl_numero_par + numero
+        c = c + 1
 
 if tpl_numero.count(9) == 1:
     plu_sing = 'vez'
@@ -12,16 +18,9 @@ print(f'\nVocê digitou os valores {tpl_numero}')
 print(f'O valor 9 apareceu {tpl_numero.count(9)} {plu_sing}')
 print(f'O valor 3 apareceu na {tpl_numero.index(3)}ª posição')
 
-c = 0
-for i in range(len(tpl_numero)):
-  if tpl_numero[i] % 2 == 0:
-    c = c + 1
-  # else:
-  #   c = 0
 if c == 0:
-  print(f'Não foi digitado nenhum valor par')
+    print(f'Não foi digitado nenhum valor par')
+elif c == 1:
+    print(f'Foi digitado apenas um número par: {tpl_numero[i]}')
 else:
-  if c == 1:
-    print(f'Foi digitado apenas um número par')
-  else:
-    print(f'Os valores pares digitados foram {c}')
+    print(f'Os valores pares digitados foram {tpl_numero_par}')
