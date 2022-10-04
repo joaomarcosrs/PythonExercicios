04426b9f538ab6205d0840778bf5146dcde4ca92
joaomarcosrs <joaomarcosrs40@gmail.com>
2022-10-04 18:19:34 -0300

    Análise de dados em uma Tupla

--- ex075.py
+++ ex075.py
@@ -16,11 +16,17 @@ else:
     plu_sing = 'vezes'
 print(f'\nVocê digitou os valores {tpl_numero}')
 print(f'O valor 9 apareceu {tpl_numero.count(9)} {plu_sing}')
-print(f'O valor 3 apareceu na {tpl_numero.index(3)}ª posição')
+
+if 3 not in tpl_numero:
+    print(f'O número 3 não aparece na sequência digitada')
+else:
+    print(f'O valor 3 apareceu na {tpl_numero.index(3)}ª posição')
 
 if c == 0:
     print(f'Não foi digitado nenhum valor par')
 elif c == 1:
     print(f'Foi digitado apenas um número par: {tpl_numero[i]}')
 else:
-    print(f'Os valores pares digitados foram {tpl_numero_par}')
+    print(f'Os valores pares digitados foram ', end='')
+    for i in range(len(tpl_numero_par)):
+        print(f'{tpl_numero_par[i]}', end=' ')
