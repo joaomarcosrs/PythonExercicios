9d69602183e8ca17c1d07be08815c4ac629d48d9
João Marcos <88893617+joaomarcosrs@users.noreply.github.com>
2022-10-05 16:14:39 -0300

    Contando vogais em Tupla

--- ex077.py
+++ ex077.py
@@ -2,8 +2,6 @@ tpl_palavras = (
 'aprender', 'programar', 'linguagem', 'python', 'curso', 'estudar', 'praticar', 'trabalhar', 'mercado', 'programador',
 'futuro')
 
-tpl_vogal = ('a', 'e', 'i', 'o', 'u')
-
 for i in range(len(tpl_palavras)):
     print(f'Na palavra {tpl_palavras[i].upper()} temos ', end='')
     palavra = tuple(tpl_palavras[i])
