5217353317d557056ae382b67f2ff81c13ef0e7f
joaomarcosrs <joaomarcosrs40@gmail.com>
2022-10-13 19:54:09 -0300

    Maior e menor valores em Tupla

--- ex074.py
+++ ex074.py
@@ -4,7 +4,7 @@ tpl_int = ()
 for i in range(0, 5):
    item = (randint(0, 11), )
    tpl_int = tpl_int + item
-print(f' Os número sorteados foram: ', end='')
+print(f'Os número sorteados foram: ', end='')
 for c in tpl_int:
   print(f'{c}', end=' ')
 print(f'\nO maior número sorteado foi: {max(tpl_int)}'
