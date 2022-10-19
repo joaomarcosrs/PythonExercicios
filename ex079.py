41eb723fbef51432b86739c8ab498bbbe8f51501
joaomarcosrs <joaomarcosrs40@gmail.com>
2022-10-19 19:09:05 -0300

    Valores únicos em uma Lista

---
+++ ex079.py
@@ -0,0 +1,20 @@
+lst_numero = list()
+keep_loop = 'S'
+
+while True:
+    numero = int(input('Digite um valor: '))
+    if numero not in lst_numero:
+        lst_numero.append(numero)
+        print('Valor adcionado com sucesso...')
+    else:
+        print('Valor duplicado! Não vou adicionar...')
+    while True:
+        keep_loop = str(input('Quer continuar? [S/N] ')).strip().upper()[:1]
+        if keep_loop not in 'SN':
+            print('Comando Inválido! Digite (S) SIM e (N) NÃO')
+        else:
+            break
+    if keep_loop in 'N':
+        break
+print(f'{"-=-"*15}')
+print(f'Você digitou  os valores {sorted(lst_numero)}')
