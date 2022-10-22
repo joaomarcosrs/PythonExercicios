267fa451d9fe782ac1026d78926100dbec3268af
joaomarcosrs <joaomarcosrs40@gmail.com>
2022-10-21 23:10:01 -0300

    Lista ordenada sem repetições

--- ex080.py
+++ ex080.py
@@ -1,21 +1,18 @@
 lst_numero = list()
-i = 0
 
 for i in range(0, 5):
     numero = int(input('Digite um valor: '))
-    if i == 0:
+    if i == 0 or numero > max(lst_numero):
         lst_numero.append(numero)
         print('Adicionado no final da lista...')
     else:
-        for c in range(0, len(lst_numero)):
-            if numero >= lst_numero[c]:
-                lst_numero.insert(i, numero)
-                p = i
+        c = 0
+        while c < len(lst_numero):
+            if numero <= lst_numero[c]:
+                lst_numero.insert(c, numero)
+                print(f'Adicionado na posição {c} da lista...')
                 break
-            else:
-                lst_numero.insert(i - 1, numero)
-                p = i - 1
-                break
-        print(f'Adicionado na posição {p} da lista...')
+            c = c + 1
 
+print(f'Os valores digitados em ordem foram {lst_numero}')
 
