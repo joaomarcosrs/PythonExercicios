c8e6b644b5b78a4676f02572c23ce9c2dfd0322b
João Marcos <88893617+joaomarcosrs@users.noreply.github.com>
2022-11-08 15:52:20 -0300

    Validando expressões matemáticas

--- ex083.py
+++ ex083.py
@@ -1,10 +1,17 @@
 lst_parenteses = list()
 
 expressao = str(input('Digite a expressão: ')).strip()
-for caractere in range(len(expressao)):
-  lst_parenteses.append(expressao[caractere])
+for caractere in expressao:
+  if caractere == '(':
+    lst_parenteses.append('(')
+  elif caractere == ')':
+    if len(lst_parenteses) > 0:
+      lst_parenteses.pop()
+    else:
+      lst_parenteses.append(')')
+      break
 
-if lst_parenteses.count('(') == lst_parenteses.count(')'):
+if len(lst_parenteses) == 0:
   print('Sua expressão está válida!')
 else:
   print('Sua expressão não é válida!')
